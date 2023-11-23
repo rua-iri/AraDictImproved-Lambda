33eb6a048835c17140034333170e775d54fc55cb
rua-iri <rory.mcguigan@protonmail.com>
2023-11-23 12:09:28 +0300

    update: implement word segmentation function

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class SegmentedWord:
         self.stem = stem
         self.suffix = suffix
 
-        
+
 
 
 def runAnalyser(arabicWord: str) -> list[str]:
@@ -33,6 +33,32 @@ def removeDiacritics(word: str) -> str:
 
 
 
+"""
+Splits a word in prefix + stem + suffix combinations. 
+find all possible combinations to make up word
+"""
+def segmentWord(word) -> set:
+    possibleSegments = set()
+    prefixLength: int = 0
+    suffixLength: int = 0
+
+    while prefixLength <= 4 and prefixLength < len(word):
+        prefix: str = word[:prefixLength]
+        stemLength: int = len(word) - prefixLength
+        suffixLength = 0
+
+        while stemLength >= 1 and suffixLength <= 6:
+            stem: str = word[prefixLength:prefixLength+stemLength]
+            suffix: str = word[prefixLength+stemLength:prefixLength+stemLength+suffixLength]
+            possibleSegments.add(SegmentedWord(prefix=prefix, stem=stem, suffix=suffix))
+            stemLength-=1
+            suffixLength+=1
+        
+        prefixLength+=1
+    
+    return possibleSegments
+
+
 
 
 
@@ -41,5 +67,4 @@ def removeDiacritics(word: str) -> str:
 blah = WordSolution('a', 'b', 'c', 'd', 'e')
 print(blah)
 print(blah.__str__())
-print(blah.__repr__())
 
